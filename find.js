87120d2ca98e02dc3a49df70969e25958c4778ca
Cyril Agosta <cyril.agosta@gmail.com>
2013-01-19 01:52:32 +0100

    add i in find

--- find.js
+++ find.js
@@ -2,7 +2,7 @@ define('Array/find', function() {
   
   Array.prototype.find = function(f) {
     for (var i = 0,n=this.length;i<n;i++) {
-      if (f(this[i])) return this[i];
+      if (f(this[i], i)) return this[i];
     }
   };
   
